5ff44bdd2af184b3930f12e61ed747b5e2565f2e
Ankit <ankit.sin099@gmail.com>
2021-09-20 13:40:35 +0530

    initial

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+
+const Header = () => {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+
+            </div>
+            <Image
+                className="object-contain"
+                src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Microsoft_Stream.svg"
+                width={100}
+                height={50}
+            />
+        </header>
+    )
+}
+
+export default Header
